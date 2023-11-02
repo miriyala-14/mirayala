4904589a63599fb512418b93bdf2c3f0650833e5
miriyala-14 <miriyalaprathyusha14@gmail.com>
2023-11-02 14:13:56 +0530

    my first java

---
+++ temp.java
@@ -0,0 +1,50 @@
+class SAP
+
+{
+   public static void main(String args[])
+        {
+                  int a=10;
+                   int b=20;
+                   int c=30;
+                   int temp=0;
+                  
+                   if(a<b)
+                                {  
+                                 temp=a;
+                                    a=b;
+                                  b=temp;
+                                 
+                                     }
+                           if(b<c)
+                                {  
+                                 temp=b;
+                                    b=c;
+                                  c=temp;
+                                     
+                                     }
+                      if(c<a)
+                                {  
+                                 temp=c;
+                                    c=a;
+                                  a=temp;
+                                     
+                                     }
+                                    
+               
+                           System.out.println(a);
+                           System.out.println(b);
+                            System.out.println(c);
+                 
+                 
+               }
+}
+                                 
+                       
+                 
+               
+
+                            
+                         
+                 
+                 
+               
---
+++ test.java
@@ -0,0 +1,19 @@
+ class Test
+{
+        public static void main(String args[ ])
+          {
+            int num1=10;
+           int  num2=30;
+           int temp=0;
+
+ System.out.println(num1);  
+ System.out.println(num2); 
+      temp=num1;
+      num1=num2;
+     num2=temp;
+
+ System.out.println(temp);   
+ System.out.println(num1);  
+  System.out.println(num2); 
+  }
+}
