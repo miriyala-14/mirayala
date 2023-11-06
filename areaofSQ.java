a64a11db2ba33cbdfe54e69be884fcd37ad0da37
miriyala-14 <miriyalaprathyusha14@gmail.com>
2023-11-06 14:09:24 +0530

    m

---
+++ areaofSQ.java
@@ -0,0 +1,11 @@
+class  Area
+{ public static void main(String args[])
+     {
+           int l=12;
+           int b=14;
+           int area =l*b;
+           int perimeter=2*(l+b);
+           System.out.println(area);
+           System.out.println(perimeter);
+        }
+}
